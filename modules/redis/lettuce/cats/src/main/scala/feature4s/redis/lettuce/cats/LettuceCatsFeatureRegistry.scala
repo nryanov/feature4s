feed253303d4ec5f1b674701b8f7ecbc5a92d5be
Nikita Ryanov <ryanov.nikita@yandex.ru>
2021-09-04 12:13:58 +0000

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=series/3.x

--- modules/redis/lettuce/cats/src/main/scala/feature4s/redis/lettuce/cats/LettuceCatsFeatureRegistry.scala
+++ modules/redis/lettuce/cats/src/main/scala/feature4s/redis/lettuce/cats/LettuceCatsFeatureRegistry.scala
@@ -1,6 +1,6 @@
 package feature4s.redis.lettuce.cats
 
-import cats.effect.{Blocker, ContextShift, Sync}
+import cats.effect.Sync
 import feature4s.effect.cats.CatsMonadError
 import feature4s.redis.lettuce.LettuceFeatureRegistry
 import io.lettuce.core.api.StatefulRedisConnection
@@ -18,7 +18,5 @@ class LettuceCatsFeatureRegistry[F[_]: ContextShift: Sync] private (
 object LettuceCatsFeatureRegistry {
   def useConnection[F[_]: ContextShift: Sync](
     connection: StatefulRedisConnection[String, String],
-    namespace: String,
-    blocker: Blocker
-  ): LettuceCatsFeatureRegistry[F] = new LettuceCatsFeatureRegistry(connection, namespace, blocker)
+    namespace: String): LettuceCatsFeatureRegistry[F] = new LettuceCatsFeatureRegistry(connection, namespace, blocker)
 }
